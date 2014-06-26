c8e270a6cd944e3d399bd70d91f7e2bdfa1f0060
Stuart Gale <bishboria@gmail.com>
2014-06-26 10:36:14 +0000

    Show which links were found in each published edition

--- found-links/hmrc_grouped_content.rb
+++ found-links/hmrc_grouped_content.rb
@@ -1,18 +1,30 @@
+require 'pp'
+
+link_matcher = /[^\(]*hmrc\.gov\.uk[^\)|^\s]*/
 answer = Edition.where(state: 'published').map do |e|
-  if e.respond_to?(:body) && e.body && e.body.include?('hmrc.gov.uk')
-    e.id
-  elsif e.respond_to?(:parts) && e.parts && e.parts.any? { |p| p.body && p.body.include?('hmrc.gov.uk') }
-    e.id
+  if e.respond_to?(:body) && e.body && (matches = e.body.scan(link_matcher))
+    matches.unshift(e.id) unless matches.empty?
+  elsif e.respond_to?(:parts) && e.parts
+    matches = []
+    e.parts.each do |p|
+      part_matches = p.body.scan(link_matcher)
+      matches.concat(part_matches) unless part_matches.empty?
+    end
+    matches.unshift(e.id) unless matches.empty?
   end
 end.compact
 
-File.open('hmrc-grouped-content.html', 'w') do |f|
-  answer.group_by {|e| Edition.find(e).class.name }.to_a.each do |editions|
-    f.puts editions.first
-    f.puts "<br/>"
+File.open('found-links/hmrc-grouped-content.html', 'w') do |f|
+  answer.group_by {|e| Edition.find(e.first).class.name }.to_a.each do |editions|
+    f.puts "<h1>#{editions.first}</h1>"
     editions.last.each do |edition|
-      url = "http://publisher.production.alphagov.co.uk/editions/#{edition}"
+      url = "http://publisher.production.alphagov.co.uk/editions/#{edition.first}"
       f.puts "<a href='#{url}'>#{url}</a><br/>"
+      f.puts "<ul>"
+      edition[1..-1].each do |link|
+        f.puts "<li><a href='#{link}'>#{link}</a></li>"
+      end
+      f.puts "</ul>"
     end
   end
 end
